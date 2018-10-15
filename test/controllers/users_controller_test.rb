532f88fb6d01c653883a4c13ef2d876112bc0994
Anibel America <ameriabel@gmail.com>
2018-10-15 16:40:20 -0700

    adds tests for user index action and adds a dependent destroy relation to votes in model

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,4 +2,21 @@ require 'test_helper'
 
 describe UsersController do
 
+  describe 'index' do
+    it 'succeeds when there are users' do
+      expect(User.count).must_be :>=, 1, "No users are set up in fixtures"
+
+      get users_path
+      must_respond_with :success
+    end
+
+    it 'succeeds when there are no users' do
+      all_user_count = User.count
+
+      expect {
+        User.destroy_all
+      }.must_change('User.count', -all_user_count)
+    end
+  end
+
 end

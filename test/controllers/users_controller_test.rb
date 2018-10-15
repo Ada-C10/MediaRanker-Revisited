19df40b033c0a7019d809b2b70d715065a56c6cf
Jazz <jazziesf21@yahoo.com>
2018-10-15 16:17:45 -0700

    test in progess

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,5 +1,29 @@
 require 'test_helper'
 
 describe UsersController do
+  it "should get index" do
+    get users_path
+
+    must_respond_with :success
+  end
+
+  describe "show" do
+
+    it "should get a book's show page" do
+      id = users(:dan).id
+
+      get user_path(id)
+
+      must_respond_with :success
+    end
+
+    it "should repond with not_found if given an invalid id" do
+      id = -1
+
+      get user_path(id)
+
+      must_respond_with :not_found
+    end
+  end
 
 end
--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,73 +1,156 @@
 require 'test_helper'
 
 describe WorksController do
-  describe "root" do
-    it "succeeds with all media types" do
+  let (:movie) {works(:movie)}
+
+   describe "root" do
+
+     it "succeeds with all media types" do
       # Precondition: there is at least one media of each category
+      get root_path
 
+      must_respond_with :success
     end
 
-    it "succeeds with one media type absent" do
+     it "succeeds with one media type absent" do
       # Precondition: there is at least one media in two of the categories
+      movie.category = "book"
 
-    end
+      get root_path
+
+      must_respond_with :success
+
+     end
 
     it "succeeds with no media" do
+      Work.destroy_all
 
+      get root_path
+
+      must_respond_with :success
+      expect(Work.all.count).must_equal 0
     end
-  end
+   end
 
   CATEGORIES = %w(albums books movies)
   INVALID_CATEGORIES = ["nope", "42", "", "  ", "albumstrailingtext"]
 
   describe "index" do
     it "succeeds when there are works" do
+      get works_path
 
+      must_respond_with :success
     end
 
     it "succeeds when there are no works" do
+      Work.destroy_all
 
+      get works_path
+
+      must_respond_with :success
+      expect(Work.all.count).must_equal 0
     end
   end
 
   describe "new" do
     it "succeeds" do
+      get new_work_path
 
+      must_respond_with :success
     end
   end
 
   describe "create" do
-    it "creates a work with valid data for a real category" do
-
+      let (:work_hash) do
+        {
+      work: {
+        title: "Eat, Love, Pray",
+        creator: "Elizabeth Gilbert",
+        description: "Memoir",
+        publication_year: 2004,
+        category: "book"
+      }
+    }
     end
 
-    it "renders bad_request and does not update the DB for bogus data" do
+    it "creates a work with valid data for a real category" do
+      expect {
+              post works_path, params: work_hash
+            }.must_change "Work.count", 1
 
+      must_respond_with :redirect
+
+      expect(Work.last.title).must_equal work_hash[:work][:title]
+      expect(Work.last.creator).must_equal work_hash[:work][:creator]
+      expect(Work.last.description).must_equal work_hash[:work][:description]
+      expect(Work.last.category).must_equal work_hash[:work][:category]
+      expect(Work.last.description).must_equal work_hash[:work][:description]
+    end
+
+
+    it "renders bad_request and does not update the DB for bogus data" do
+      work_hash = {
+          work: {
+            creator: "Elizabeth Gilbert",
+            description: "Memoir",
+            publication_year: 2004,
+            category: "book"
+          }
+        }
+
+      expect {
+              post works_path, params: work_hash
+            }.wont_change "Work.count"
+
+      must_respond_with :bad_request
     end
 
     it "renders 400 bad_request for bogus categories" do
+      work_hash[:work][:category] = INVALID_CATEGORIES.first
+
+      expect {
+              post works_path, params: work_hash
+            }.wont_change "Work.count"
+
+      must_respond_with :bad_request
 
     end
 
   end
 
   describe "show" do
-    it "succeeds for an extant work ID" do
+    it "succeeds for an existing work ID" do
+      id = works(:poodr).id
 
+      get work_path(id)
+
+      must_respond_with :success
     end
 
     it "renders 404 not_found for a bogus work ID" do
+      id = -1
 
+      get work_path(id)
+
+      must_respond_with :not_found
     end
   end
 
   describe "edit" do
-    it "succeeds for an extant work ID" do
+    it "succeeds for an existing work ID" do
+      id = works(:poodr).id
 
+      get edit_work_path(id)
+
+      must_respond_with :success
     end
 
     it "renders 404 not_found for a bogus work ID" do
+      id = -1
 
+      get work_path(id)
+
+      must_respond_with :not_found
     end
   end
 
@@ -75,42 +158,42 @@ describe WorksController do
     it "succeeds for valid data and an extant work ID" do
 
     end
-
-    it "renders bad_request for bogus data" do
-
-    end
-
-    it "renders 404 not_found for a bogus work ID" do
-
-    end
-  end
-
-  describe "destroy" do
-    it "succeeds for an extant work ID" do
-
-    end
-
-    it "renders 404 not_found and does not update the DB for a bogus work ID" do
-
-    end
-  end
-
-  describe "upvote" do
-
-    it "redirects to the work page if no user is logged in" do
-
-    end
-
-    it "redirects to the work page after the user has logged out" do
-
-    end
-
-    it "succeeds for a logged-in user and a fresh user-vote pair" do
-
-    end
-
-    it "redirects to the work page if the user has already voted for that work" do
-
-    end
-  end
+  #
+  #   it "renders bad_request for bogus data" do
+  #
+  #   end
+  #
+  #   it "renders 404 not_found for a bogus work ID" do
+  #
+  #   end
+  # end
+  #
+  # describe "destroy" do
+  #   it "succeeds for an extant work ID" do
+  #
+  #   end
+  #
+  #   it "renders 404 not_found and does not update the DB for a bogus work ID" do
+  #
+  #   end
+  # end
+  #
+  # describe "upvote" do
+  #
+  #   it "redirects to the work page if no user is logged in" do
+  #
+  #   end
+  #
+  #   it "redirects to the work page after the user has logged out" do
+  #
+  #   end
+  #
+  #   it "succeeds for a logged-in user and a fresh user-vote pair" do
+  #
+  #   end
+  #
+  #   it "redirects to the work page if the user has already voted for that work" do
+  #
+  #   end
+   # end
 end

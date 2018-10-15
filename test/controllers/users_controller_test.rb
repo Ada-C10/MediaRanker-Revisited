e0921559358c4e285fda2f0099c48176e4bd2421
Katie Jahanmir <katietj@gmail.com>
2018-10-15 14:23:30 -0700

    users testing done

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,5 +1,25 @@
 require 'test_helper'
 
 describe UsersController do
+  it "should get index" do
+    get users_path
+    must_respond_with :success
+  end
+
+  describe "show" do
+    it "should get a user's show page" do
+      id = users(:dan).id
+
+      get user_path(id)
+
+      must_respond_with :success
+    end
+
+    it "should respond with not found if given an invalid user id" do
+      id = -2
+      get user_path(id)
+      must_respond_with :not_found
+    end
+  end
 
 end

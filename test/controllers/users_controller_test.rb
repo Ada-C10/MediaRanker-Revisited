400729a742ea832ad40543cd82d2d787fbec0809
Carly Reiter <yepodacarly@gmail.com>
2018-10-15 14:44:33 -0700

    changed the invalid user to last + 1 because ran into trouble deleting a user (:dan) who had votes.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -16,28 +16,18 @@ describe UsersController do
   describe "show" do
 
     it "should respond with success for showing an existing user" do
-      # Arrange
-      existing_user = books(:dan)
 
-      # Act
+      existing_user = users(:dan)
+
       get user_path(existing_user.id)
 
-      # Assert
       must_respond_with :success
     end
 
     it "should respond with 404 not found for showing a non-existing user" do
-      # Arrange
-      user = user(:dan)
-      id = user.id
 
-      get user_path(id)
-      user.destroy
+      get user_path(User.last.id + 1)
 
-      # Act
-      get user_path(id)
-
-      # Assert
       must_respond_with 404
 
     end

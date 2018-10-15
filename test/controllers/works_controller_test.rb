c3430c373a4f0e4a3b3c9dbb55af8f7b1d90eaa4
Aurea <fu.sakura@gmail.com>
2018-10-15 14:18:24 -0700

    add work controller tests for methods except update, upvote, and destroy

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,19 +1,54 @@
 require 'test_helper'
 
 describe WorksController do
+  let (:delete_all) {
+    Work.all.each do |work|
+      work.destroy
+    end
+  }
+
+  let (:work_data) {
+    work_data = {
+      work: {
+        title: "new test work",
+        category: "album"
+      }
+    }
+  }
+
+  let (:existing_work) {
+    existing_work = works(:poodr)
+  }
+
   describe "root" do
     it "succeeds with all media types" do
       # Precondition: there is at least one media of each category
 
+      get root_path
+
+      must_respond_with :success
     end
 
     it "succeeds with one media type absent" do
       # Precondition: there is at least one media in two of the categories
 
+      movies = Work.where(category: 'movie')
+      movies.each do |movie|
+        movie.destroy
+      end
+
+      get root_path
+
+      must_respond_with :success
     end
 
     it "succeeds with no media" do
 
+      delete_all
+
+      get root_path
+
+      must_respond_with :success
     end
   end
 
@@ -23,57 +58,114 @@ describe WorksController do
   describe "index" do
     it "succeeds when there are works" do
 
+      get works_path
+
+      must_respond_with :success
+
     end
 
     it "succeeds when there are no works" do
+      delete_all
 
+      get works_path
+
+      must_respond_with :success
     end
   end
 
   describe "new" do
     it "succeeds" do
+      get new_work_path
 
+      must_respond_with :success
     end
   end
 
   describe "create" do
     it "creates a work with valid data for a real category" do
 
+      CATEGORIES.each do |category|
+
+        work_data[:work][:category] = category
+        test_work = Work.new(work_data[:work])
+        test_work.must_be :valid?, "Work data was invalid. Please fix."
+
+        expect{
+          post works_path, params: work_data
+        }.must_change('Work.count', +1)
+
+        must_redirect_to work_path(Work.last)
+      end
+
     end
 
     it "renders bad_request and does not update the DB for bogus data" do
+      work_data[:work][:title] = nil
 
+      test_work = Work.new(work_data[:work])
+      test_work.wont_be :valid?, "Work data wasn't invalid. Please fix."
+
+      expect{
+        post works_path, params: work_data
+      }.wont_change('Work.count')
+
+      must_respond_with :bad_request
     end
 
     it "renders 400 bad_request for bogus categories" do
 
+      INVALID_CATEGORIES.each do |category|
+        work_data[:work][:category] = category
+
+        test_work = Work.new(work_data[:work])
+        test_work.wont_be :valid?, "Work data wasn't invalid. Please fix."
+
+        expect{
+          post works_path, params: work_data
+        }.wont_change('Work.count')
+
+        must_respond_with :bad_request
+      end
     end
 
   end
 
   describe "show" do
-    it "succeeds for an extant work ID" do
 
+    it "succeeds for an extant work ID" do
+      get work_path(existing_work.id)
+
+      must_respond_with :success
     end
 
     it "renders 404 not_found for a bogus work ID" do
+      existing_work.destroy
 
+      get work_path(existing_work.id)
+
+      must_respond_with :not_found
     end
   end
 
   describe "edit" do
     it "succeeds for an extant work ID" do
+      get edit_work_path(existing_work.id)
 
+      must_respond_with :success
     end
 
     it "renders 404 not_found for a bogus work ID" do
+      existing_work.destroy
 
+      get edit_work_path(existing_work.id)
+
+      must_respond_with :not_found
     end
   end
 
   describe "update" do
     it "succeeds for valid data and an extant work ID" do
-
+      
     end
 
     it "renders bad_request for bogus data" do

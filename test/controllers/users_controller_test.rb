f53db49cb7c4990ba845016f17645b2e581285e9
Hayden Williams <hwilliams.deva@gmail.com>
2018-10-15 14:14:30 -0700

    add tests for users_controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,4 +2,29 @@ require 'test_helper'
 
 describe UsersController do
 
+  describe 'index' do
+
+    it 'should get index' do
+      get users_path
+      must_respond_with :success
+    end
+
+  end
+
+  describe 'show' do
+
+    it 'gets the show page for valid id' do
+      id = users(:dan).id
+      get user_path(id)
+      must_respond_with :success
+    end
+
+    it 'responds not_found for invalid id' do
+      invalid_id = User.last.id + 1
+      get user_path(invalid_id)
+      must_respond_with :not_found
+    end
+
+  end
+
 end

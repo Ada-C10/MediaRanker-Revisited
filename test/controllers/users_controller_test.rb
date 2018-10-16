68450fa6638da25357c800fb60e3b0bc017b647b
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-16 16:19:53 -0700

    Added user fixtures

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -36,5 +36,22 @@ describe UsersController do
 
     end
   end
+  describe "auth_callback" do
+    it "logs in an existing user and redirects to root route" do
+      start_count = User.count
 
+      user = users(:grace)
+
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+
+      get auth_callback_path(:github)
+
+      must_redirect_to root_path
+
+      session[:user_id].must_equal user.id
+
+      User.count.must_equal start_count
+    end
+
+  end
 end

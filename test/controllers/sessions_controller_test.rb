42311b1175ae6ab4e3e04aa7c6fb6bbd3100cd41
Daniela Di Iorio <danielad@gmail.com>
2018-10-15 19:44:35 -0700

    sessions controller tests, wave 1 complete

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,4 +2,47 @@ require "test_helper"
 
 describe SessionsController do
 
+  describe 'login_form' do
+    it 'successfully gets the login form' do
+      get login_path
+
+      must_respond_with :success
+    end
+  end
+
+  describe 'login' do
+
+    it 'logs in successfully with a username already in the db' do
+      expect {
+        post login_path, params: {username: 'kari'}
+      }.wont_change 'User.count'
+
+      expect(session[:user_id]).must_equal users(:kari).id
+
+      must_redirect_to root_path
+    end
+
+    it 'makes a new user and logs that user in if not already in the db' do
+      expect {
+        post login_path, params: {username: 'bonkers'}
+      }.must_change 'User.count', 1
+
+      expect(session[:user_id]).wont_equal nil
+
+      must_redirect_to root_path
+    end
+  end
+
+  describe 'logout' do
+    it 'nullifies user id in session' do
+      post login_path, params: {username: 'kari'}
+      expect(session[:user_id]).must_equal users(:kari).id
+
+      post logout_path
+      expect(session[:user_id]).must_equal nil
+
+      must_redirect_to root_path
+    end
+  end
+
 end

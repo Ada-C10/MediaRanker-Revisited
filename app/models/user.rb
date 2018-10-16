4938e3ef274e5407e1b1311d1b4780285c44cc4e
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-16 13:57:17 -0700

    temp solution for resolving if username does not exist in User.build_from_github(auth_hash) method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,11 @@ class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true
 
   def self.build_from_github(auth_hash)
-    User.new(provider: auth_hash['provider'], uid: auth_hash['uid'], username: auth_hash['info']['nickname'], name: auth_hash['info']['name'], email: auth_hash['info']['email'])
+    user = User.new(provider: auth_hash['provider'], uid: auth_hash['uid'], username: auth_hash['info']['nickname'], name: auth_hash['info']['name'], email: auth_hash['info']['email'])
+    if auth_hash['info'] == nil
+
+      #temp solution for resolving if username does not exist 
+      user.username = "#{auth_hash['info']['name']}#{auth_hash['uid']}"
+    end
   end
 end

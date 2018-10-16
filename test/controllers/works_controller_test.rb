1121a460214d481c2095a834752cc2919e403b92
Pushpa Agr <pushpaagr108@gmail.com>
2018-10-16 15:42:49 -0700

    finished, root, index, create, and show for controller testing

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -32,8 +32,6 @@ describe WorksController do
     end
   end
 
-  CATEGORIES = %w(albums books movies)
-  INVALID_CATEGORIES = ["nope", "42", "", "  ", "albumstrailingtext"]
 
   describe "index" do
     it "succeeds when there are works" do
@@ -59,101 +57,159 @@ describe WorksController do
   end
 
 
-describe "create" do
-  it "creates a work with valid data for a real category" do
-        # Arrange
-        work_data = {
-          work: {
-            title: "New  Movie",
-            category: "movie"
+  describe "create" do
+    it "creates a work with valid data for a real category" do
+      # Arrange
+      work_data = {
+        work: {
+          title: "New  Movie",
+          category: "movie"
 
-          }
         }
+      }
 
-        # Assumptions
-        test_movie = Work.new(work_data[:work])
-        test_movie.must_be :valid?, "Work data was invalid. Please come fix this test"
+      # Assumptions
+      test_movie = Work.new(work_data[:work])
+      test_movie.must_be :valid?, "Work data was invalid. Please come fix this test"
 
-        # Act
-        expect {
-          post works_path, params: work_data
-        }.must_change('Work.count', +1)
+      # Act
+      expect {
+        post works_path, params: work_data
+      }.must_change('Work.count', +1)
 
-        # Assert
-        must_redirect_to work_path(Work.last.id)
+      # Assert
+      must_redirect_to work_path(Work.last.id)
+    end
 
-  end
 
-  it "renders bad_request and does not update the DB for bogus data" do
+    it "renders bad_request and does not update the DB for bogus data" do
+      work_data = {
+        work: {
+          title: Work.first.title,
+          category:  Work.first.category
 
-  end
+        }
+      }
+      # Assumptions
+      Work.new(work_data[:work]).wont_be :valid?, "Work data wasn't invalid, please fix it"
 
-  it "renders 400 bad_request for bogus categories" do
+      # Act
+      expect {
+        post works_path, params: work_data
+      }.wont_change('Work.count')
 
-  end
+      # Assert
+      must_respond_with :bad_request
 
-end
+    end
 
-describe "show" do
-  it "succeeds for an extant work ID" do
 
-  end
+    CATEGORIES = %w(albums books movies)
+    INVALID_CATEGORIES = ["nope", "42", "", "  ", "albumstrailingtext"]
 
-  it "renders 404 not_found for a bogus work ID" do
+    it "renders 400 bad_request for bogus categories" do
 
-  end
-end
+      work_data = {
+        work: {
+          title: "Mangoes",
+          category:  INVALID_CATEGORIES[0]
 
-describe "edit" do
-  it "succeeds for an extant work ID" do
+        }
+      }
+      # Assumptions
+      Work.new(work_data[:work]).wont_be :valid?, "Work data wasn't invalid, please fix it"
 
-  end
+      # Act
+      expect {
+        post works_path, params: work_data
+      }.wont_change('Work.count')
 
-  it "renders 404 not_found for a bogus work ID" do
-
-  end
-end
-
-describe "update" do
-  it "succeeds for valid data and an extant work ID" do
-
-  end
-
-  it "renders bad_request for bogus data" do
-
-  end
-
-  it "renders 404 not_found for a bogus work ID" do
-
-  end
-end
-
-describe "destroy" do
-  it "succeeds for an extant work ID" do
-
-  end
-
-  it "renders 404 not_found and does not update the DB for a bogus work ID" do
-
-  end
-end
-
-describe "upvote" do
-
-  it "redirects to the work page if no user is logged in" do
+      # Assert
+      must_respond_with :bad_request
+    end
 
   end
 
-  it "redirects to the work page after the user has logged out" do
+  describe "show" do
+    it "succeeds for an extant work ID" do
 
+      # Arrange
+      exisiting_work = works(:album)
+
+      # Act
+      get work_path(exisiting_work.id)
+
+      # Assert
+      must_respond_with :success
+    end
+
+    it "renders 404 not_found for a bogus work ID" do
+      work = works(:album)
+
+      id = work.id
+
+      get work_path(id)
+      must_respond_with :success
+
+      work.destroy #better to do it this way then some random number like book with id of
+
+      get work_path(id)
+
+      must_respond_with :missing #can do :not_found or 404
+
+    end
   end
 
-  it "succeeds for a logged-in user and a fresh user-vote pair" do
+  describe "edit" do
+    it "succeeds for an extant work ID" do
 
+    end
+
+    it "renders 404 not_found for a bogus work ID" do
+
+    end
   end
 
-  it "redirects to the work page if the user has already voted for that work" do
+  describe "update" do
+    it "succeeds for valid data and an extant work ID" do
 
+    end
+
+    it "renders bad_request for bogus data" do
+
+    end
+
+    it "renders 404 not_found for a bogus work ID" do
+
+    end
   end
-end
+
+  describe "destroy" do
+    it "succeeds for an extant work ID" do
+
+    end
+
+    it "renders 404 not_found and does not update the DB for a bogus work ID" do
+
+    end
+  end
+
+  describe "upvote" do
+
+    it "redirects to the work page if no user is logged in" do
+
+    end
+
+    it "redirects to the work page after the user has logged out" do
+
+    end
+
+    it "succeeds for a logged-in user and a fresh user-vote pair" do
+
+    end
+
+    it "redirects to the work page if the user has already voted for that work" do
+
+    end
+  end
 end

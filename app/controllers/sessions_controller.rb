381b18ae3491a55bdb355f138b754e256d11c46a
Laura <larmartinez2@gmail.com>
2018-10-16 16:32:47 -0700

    logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -31,6 +31,7 @@ class SessionsController < ApplicationController
   #   flash[:result_text] = "Successfully logged out"
   #   redirect_to root_path
   # end
+
   def create
     auth_hash = request.env['omniauth.auth']
 
@@ -43,7 +44,6 @@ class SessionsController < ApplicationController
       # User doesn't match anything in the DB
       # Attempt to create a new user
       user = User.build_from_github(auth_hash)
-      binding.pry
 
       if user.save
         flash[:success] = "Logged in as new user #{user.name}"
@@ -62,6 +62,13 @@ class SessionsController < ApplicationController
 
     # If we get here, we have a valid user instance
     session[:user_id] = user.id
+    redirect_to root_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out!"
+
     redirect_to root_path
   end
 end

d4a98092c4c67e32c2712c13110b74e66805f15b
Aurea <fu.sakura@gmail.com>
2018-10-16 14:12:11 -0700

    restrict permission to all webpages except root for non logged in guests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :find_user
+  before_action :is_logged_in
 
   def render_404
     # DPR: this will actually render a 404 page in production
@@ -12,6 +13,14 @@ private
   def find_user
     if session[:user_id]
       @login_user = User.find_by(id: session[:user_id])
+    end
+  end
+
+  def is_logged_in
+    unless @login_user
+      flash[:status] = :failure
+      flash[:result_text] = "Unauthorized to access page. Please log in."
+      redirect_to root_path
     end
   end
 end

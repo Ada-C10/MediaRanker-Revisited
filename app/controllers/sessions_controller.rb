aa8b507475792124b8ea4f107d57452165c9697f
Melissa O'Hearn <melissa.ohearn@gmail.com>
2018-10-16 14:02:25 -0700

    add username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -35,7 +35,6 @@ class SessionsController < ApplicationController
   def create
 
     auth_hash = request.env['omniauth.auth']
-
     user = User.find_by(uid: auth_hash[:uid], provider: 'github')
     if user
       # User was found in the database

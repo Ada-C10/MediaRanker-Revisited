b3d67bc93ea2d0a0adb29e3612e94ba51917d27b
Laura <larmartinez2@gmail.com>
2018-10-16 16:25:20 -0700

    log in works

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'works#root'
   get "/auth/:provider/callback", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy", as: "logout"
   resources :works
   post '/works/:id/upvote', to: 'works#upvote', as: 'upvote'
 

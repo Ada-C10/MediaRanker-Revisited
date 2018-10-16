6babfb10f11f18181404f4bbe815f24b9fccbea7
Laura <larmartinez2@gmail.com>
2018-10-15 17:06:01 -0700

    test the root path

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -3,17 +3,26 @@ require 'test_helper'
 describe WorksController do
   describe "root" do
     it "succeeds with all media types" do
-      # Precondition: there is at least one media of each category
-
+      get works_path
+      must_respond_with :success
     end
 
     it "succeeds with one media type absent" do
       # Precondition: there is at least one media in two of the categories
+      works(:movie).destroy
+      get works_path
 
+      must_respond_with :success
     end
 
     it "succeeds with no media" do
 
+      all = Work.all
+      all.each do |item|
+        item.destroy
+      end
+      get works_path
+      must_respond_with :success
     end
   end
 

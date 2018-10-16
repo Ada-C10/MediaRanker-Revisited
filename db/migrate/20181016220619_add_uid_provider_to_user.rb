158d4fb7f70e991c4caaffcac7beb00ef8069f69
Daniela Di Iorio <danielad@gmail.com>
2018-10-16 15:11:08 -0700

    added uid and provider to user model, added gems

---
+++ db/migrate/20181016220619_add_uid_provider_to_user.rb
@@ -0,0 +1,6 @@
+class AddUidProviderToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :uid, :integer, null: false
+    add_column :users, :provider, :string, null: false
+  end
+end

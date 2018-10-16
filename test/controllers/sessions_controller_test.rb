eefb47e4dac854b3f14c798ccb9b5cdfc491319c
Jessie Zhang <zhang_jessie2017@outlook.com>
2018-10-16 14:40:43 -0700

    work controller test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,5 +1,12 @@
 require "test_helper"
 
 describe SessionsController do
+  describe "logout" do
+    post login_path params: {username:'dan'}
+    expect(session[:user_id]).wont_be_nil
+
+    delete logout_path
+    expect(session[:user_id]).to_equal nil
+
 
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,5 +1,51 @@
 require 'test_helper'
 
 describe UsersController do
+  let(:dan) {users(:dan)}
+  let(:kari) {users(:kari)}
+  let(:one) {votes(:one)}
+  let(:two) {votes(:two)}
+  let(:three) {votes(:three)}
+
+  describe "index" do
+    it "succeeds when there are users" do
+
+      get users_path
+      must_respond_with :success
+
+    end
+
+    it "succeeds when there are no users" do
+      one.destroy
+      two.destroy
+      three.destroy
+      dan.destroy
+      kari.destroy
+
+
+      expect(User.all).must_equal []
+
+      get users_path
+      must_respond_with :success
+    end
+  end
+
+  describe "show" do
+    it "succeeds for an extant user ID" do
+
+      get user_path(dan.id)
+
+      must_respond_with :success
+
+    end
+
+    it "renders 404 not_found for a bogus user ID" do
+     id = -1
+     get user_path(id)
+
+     must_respond_with :not_found
+
+    end
+  end
 
 end

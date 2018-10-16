e68fb0f48a2e5f7ef0f86a15dee4e5b938b0650a
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-10-15 18:26:21 -0700

    add passing tests for user controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,5 +1,38 @@
 require 'test_helper'
 
 describe UsersController do
+  describe "index" do
+    it "succeeds when there is user data" do
+      get users_path
 
+      must_respond_with :success
+    end
+
+    it "succeeds when there are no users" do
+      Vote.delete_all
+      User.delete_all
+
+      expect(User.count).must_equal 0
+
+      get users_path
+      must_respond_with :success
+    end
+  end
+
+  describe "show" do
+    it "succeeds in showing a user for a given id" do
+      id = users(:jackie).id
+      get user_path(id)
+
+      must_respond_with :success
+    end
+
+    it "renders 404 not found when a given id is not available" do
+      id = -1
+
+      get user_path(id)
+
+      must_respond_with :not_found
+    end
+  end
 end

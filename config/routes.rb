9730b3c8cdebddbca959cbc8440aec0cbc4a1c5c
Katricia Smith <smithkatricia@gmail.com>
2018-10-16 15:02:00 -0700

    Uncomment sessions#logout route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'works#root'
   # get '/login', to: 'sessions#login_form', as: 'login'
   # post '/login', to: 'sessions#login'
-  # post '/logout', to: 'sessions#logout', as: 'logout'
+  post '/logout', to: 'sessions#logout', as: 'logout'
   get "/auth/:provider/callback", to: "sessions#create"
 
   resources :works

af9118311b8d6794a61d762603ff70a7593c4f0c
Katricia Smith <smithkatricia@gmail.com>
2018-10-16 14:59:06 -0700

    Edit flash messages. Uncomment logout method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
-    user = User.find_by(uid: auth_hash[:uid], provider: 'github')
+    user = User.find_by(uid: auth_hash[:uid], provider: auth_hash[:provider])
 
     if user
-      flash[:success] = "Logged in as returning user #{user.name}"
+      flash[:success] =  flash[:success] = "Logged in as returning user #{user.name}"
     else
       flash[:notice] = "Log in not successful."
       # User doesn't match anything in the DB
@@ -14,21 +14,28 @@ class SessionsController < ApplicationController
       user = User.add_user_from_github(auth_hash)
 
       if user.save
-        flash[:success] = "Logged in as a new user #{user.username}"
+        flash[:success] = "Logged in as a new user"
       else
         flash[:error] = "Could not create new user account #{user.errors.messages}"
         redirect_to root_path
         return
-      end 
+      end
     end
 
     session[:user_id] = user.id
     redirect_to root_path
   end
+
+  def logout
+    session[:user_id] = nil
+    flash[:status] = :success
+    flash[:result_text] = "Successfully logged out"
+    redirect_to root_path
+  end
 end
 
 
-# Code for login and logout w/o OAuth
+## Code for login and logout w/o OAuth
 # def login_form
 # end
 #

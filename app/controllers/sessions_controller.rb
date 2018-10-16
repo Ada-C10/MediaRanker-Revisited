9d40b92e450ab712ffcc66337f006fca6ff6c6be
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-16 14:59:07 -0700

    Created build user method, added create method to session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,25 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
-    raise
-  end 
+
+    user = User.find_by(uid: auth_hash[:uid], provider: 'github')
+
+    if user
+      flash[:success] = "Logged in as returning user #{user.username}"
+    else
+      user = User.build_from_github(auth_hash)
+
+      if user.save
+        flash[:success] = "Logged in as new user #{user.username}"
+      else
+        flash[:error] = "Could not create new user account: #{user.errors.messages}"
+        redirect_to root_path
+        return
+      end
+    end
+    session[:user_id] = user.id
+    redirect_to root_path
+  end
 
   def login_form
   end

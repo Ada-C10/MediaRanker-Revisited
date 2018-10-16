0b6852538599a1419aab15c1da4475a165e8ca08
Jazz <jazziesf21@yahoo.com>
2018-10-15 17:18:47 -0700

    Wave1 test in progress

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -155,34 +155,103 @@ describe WorksController do
   end
 
   describe "update" do
-    it "succeeds for valid data and an extant work ID" do
+        let (:work_hash) do
+          {
+        work: {
+          title: "Eat, Love, Pray",
+          creator: "Elizabeth Gilbert",
+          description: "Memoir",
+          publication_year: 2004,
+          category: "book"
+        }
+      }
+    end
+
+    it "succeeds for valid data and an existing work ID" do
+      id = works(:poodr).id
+
+      expect {
+        patch work_path(id), params: work_hash
+      }.wont_change "Work.count"
+
+      must_respond_with :redirect
+      new_work = Work.find_by(id: id)
+
+      expect(new_work.title).must_equal work_hash[:work][:title]
+      expect(new_work.creator).must_equal work_hash[:work][:creator]
+      expect(new_work.description).must_equal work_hash[:work][:description]
+      expect(new_work.category).must_equal work_hash[:work][:category]
+      expect(new_work.description).must_equal work_hash[:work][:description]
+    end
+
+    it "renders bad_request for bogus data" do
+      work_hash[:work][:title] = nil
+      id = works(:poodr).id
+      old_poodr = works(:poodr)
+
+      expect {
+        patch work_path(id), params: work_hash
+      }.wont_change "Work.count"
+
+      new_poodr = Work.find(id)
+
+      must_respond_with :bad_request
+      expect(old_poodr.title).must_equal new_poodr.title
+      expect(old_poodr.creator).must_equal new_poodr.creator
+      expect(old_poodr.description).must_equal new_poodr.description
+      expect(old_poodr.creator).must_equal new_poodr.creator
+      expect(old_poodr.description).must_equal new_poodr.description
 
     end
-  #
-  #   it "renders bad_request for bogus data" do
-  #
-  #   end
-  #
-  #   it "renders 404 not_found for a bogus work ID" do
-  #
-  #   end
-  # end
-  #
-  # describe "destroy" do
-  #   it "succeeds for an extant work ID" do
-  #
-  #   end
-  #
-  #   it "renders 404 not_found and does not update the DB for a bogus work ID" do
-  #
-  #   end
-  # end
-  #
-  # describe "upvote" do
-  #
-  #   it "redirects to the work page if no user is logged in" do
-  #
-  #   end
+
+    it "renders 404 not_found for a bogus work ID" do
+        id = -1
+
+        expect {
+          patch work_path(id)
+        }.wont_change 'Work.count'
+
+        must_respond_with :not_found
+    end
+  end
+
+  describe "destroy" do
+    it "succeeds for an existing work ID" do
+      id = works(:poodr).id
+      media = works(:poodr).category
+
+      expect {
+       delete work_path(id)
+     }.must_change 'Work.count', -1
+
+      must_respond_with :redirect
+      expect(flash[:result_text]).must_equal "Successfully destroyed #{media} #{id}"
+      expect(Work.find_by(id: id)).must_equal nil
+    end
+
+    it "renders 404 not_found and does not update the DB for a bogus work ID" do
+      id = -1
+
+      expect {
+        delete work_path(id)
+      }.wont_change 'Work.count'
+
+      must_respond_with :not_found
+    end
+  end
+
+  describe "upvote" do
+
+    it "redirects to the work page if no user is logged in" do
+      id = works(:poodr).id
+
+      expect {
+        post upvote_path(id)
+      }.wont_change 'Vote.count'
+
+      must_respond_with :redirect
+    end
+  end
   #
   #   it "redirects to the work page after the user has logged out" do
   #

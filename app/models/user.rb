2f349c98c149ca0dbc1575862cbb70267fa8f7b5
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-10-16 14:45:35 -0700

    added oauth-github

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   has_many :ranked_works, through: :votes, source: :work
 
   validates :username, uniqueness: true, presence: true
+
+  def self.new_user(auth_hash)
+    new_user = User.new(name: auth_hash[:info][:name],
+                        username: auth_hash[:info][:nickname],
+                        uid: auth_hash[:uid],
+                        provider: 'github')
+
+    return new_user
+  end
 end

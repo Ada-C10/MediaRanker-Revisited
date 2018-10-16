cd8b0f0f03a6fc7d71aedd27740f62c470f2d4ca
Alice Hsiao <alice.hsiao87@gmail.com>
2018-10-16 14:43:52 -0700

    Configured Oauth and refactored sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,16 @@
 class SessionsController < ApplicationController
-  def login_form
-  end
+  def create
+    auth_hash = request.env['omniauth.auth']
 
-  def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
+    user = User.find_by(uid: auth_hash[:uid], provider: "github")
+    if user
+      flash[:success] = "Logged in as returning user #{user.username}"
       session[:user_id] = user.id
       flash[:status] = :success
       flash[:result_text] = "Successfully logged in as existing user #{user.username}"
     else
-      user = User.new(username: username)
+      user = User.oauth_build_from_github(auth_hash)
+
       if user.save
         session[:user_id] = user.id
         flash[:status] = :success
@@ -18,17 +19,19 @@ class SessionsController < ApplicationController
         flash.now[:status] = :failure
         flash.now[:result_text] = "Could not log in"
         flash.now[:messages] = user.errors.messages
-        render "login_form", status: :bad_request
+        redirect_to root_path
         return
       end
     end
+
     redirect_to root_path
   end
 
-  def logout
+  def destroy
     session[:user_id] = nil
     flash[:status] = :success
     flash[:result_text] = "Successfully logged out"
+
     redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   has_many :ranked_works, through: :votes, source: :work
 
   validates :username, uniqueness: true, presence: true
+  validates :uid, presence: true
+  validates :provider, presence: true
+
+  def self.oauth_build_from_github(auth_hash)
+    user = User.new
+
+    user.username = auth_hash[:info][:name]
+    user.email = auth_hash[:info][:email]
+    user.uid = auth_hash[:uid]
+    user.provider = 'github'
+
+    return user
+  end
 end

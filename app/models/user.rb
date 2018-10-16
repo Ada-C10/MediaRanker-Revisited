053f382b9a9b3d4809a9177e1a3b9e1709def4d9
Anibel America <ameriabel@gmail.com>
2018-10-16 12:26:43 -0700

    adds OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,19 @@ class User < ApplicationRecord
   has_many :votes
   has_many :ranked_works, through: :votes, source: :work
 
-  validates :username, uniqueness: true, presence: true
+  validates :username, uniqueness: true
+  validates :name, uniqueness: true, presence: true
+
+  def self.build_from_github(auth_hash)
+    user = User.new(
+      username: auth_hash['info']['nickname'],
+      name: auth_hash['info']['name'],
+      email: auth_hash['info']['email'],
+      uid: auth_hash[:uid],
+      provider: 'github'
+    )
+
+    return user
+  end
+
 end

474b6b113072e55056c883ee13040dd50be1c0c5
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-10-16 17:37:24 -0700

    user prevented from viewing pages when not logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,36 +1,9 @@
 class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:create]
+
   def login_form
   end
-  #
-  # def login
-  #   username = params[:username]
-  #   if username and user = User.find_by(username: username)
-  #     session[:user_id] = user.id
-  #     flash[:status] = :success
-  #     flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-  #   else
-  #     user = User.new(username: username)
-  #     if user.save
-  #       session[:user_id] = user.id
-  #       flash[:status] = :success
-  #       flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
-  #     else
-  #       flash.now[:status] = :failure
-  #       flash.now[:result_text] = "Could not log in"
-  #       flash.now[:messages] = user.errors.messages
-  #       render "login_form", status: :bad_request
-  #       return
-  #     end
-  #   end
-  #   redirect_to root_path
-  # end
-  #
-  # def logout
-  #   session[:user_id] = nil
-  #   flash[:status] = :success
-  #   flash[:result_text] = "Successfully logged out"
-  #   redirect_to root_path
-  # end
+  
 
   def create
     auth_hash = request.env['omniauth.auth']
@@ -42,7 +15,7 @@ class SessionsController < ApplicationController
       if user.save
         flash[:success] = "Created user #{user.username}"
       else
-        flash[:error] = "Could not create new user account: #{user.errors.messages}"
+        flash[:warning] = "Could not create new user account: #{user.errors.messages}"
         redirect_to root_path
       end
     end

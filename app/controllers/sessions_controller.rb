ed66def0d04628bd82c4644d419a0062029f0f06
Alice Hsiao <alice.hsiao87@gmail.com>
2018-10-19 10:47:40 -0700

    Add omniauth test helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:create]
+
   def create
     auth_hash = request.env['omniauth.auth']
 
     user = User.find_by(uid: auth_hash[:uid], provider: "github")
     if user
-      flash[:success] = "Logged in as returning user #{user.username}"
       session[:user_id] = user.id
       flash[:status] = :success
       flash[:result_text] = "Successfully logged in as existing user #{user.username}"

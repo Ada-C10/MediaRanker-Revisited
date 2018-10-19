c837ebc976e90525e1025a17c94d1563abbaf97e
Aurea <fu.sakura@gmail.com>
2018-10-19 11:27:36 -0700

    add valid login test for sessions controller

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,5 +1,15 @@
 require "test_helper"
 
 describe SessionsController do
+  it "redirects to home page, set user id on session, and displays a successful flash message when logging in with github" do
 
+    kari = users(:kari)
+
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(kari))
+
+    get callback_path(:github)
+
+    must_redirect_to root_path
+    expect(session[:user_id]).must_equal kari.id
+  end
 end

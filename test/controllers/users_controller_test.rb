8459e0057565864b0c57ca56618d83eeb4e11eb2
Valerie Gidzinski <valgidzi@gmail.com>
2018-10-27 23:40:13 -0700

    Add tests for UsersController

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,5 +1,42 @@
 require 'test_helper'
 
 describe UsersController do
+  let (:dan) {users(:dan)}
+  let (:kari) {users(:kari)}
 
+  before do
+    Vote.all.each do |vote|
+      vote.destroy
+    end
+  end
+
+  describe 'index' do
+    it 'succeeds when there are users' do
+      get users_path
+      must_respond_with :success
+    end
+
+    it 'succeeds when there are no users' do
+
+      dan.destroy
+      kari.destroy
+
+      get users_path
+      must_respond_with :success
+    end
+  end
+
+  describe 'show' do
+    it 'succeeds when user exists' do
+      get user_path(dan)
+      must_respond_with :success
+    end
+
+    it 'renders 404 if user does not exist' do
+      dan.destroy
+
+      get user_path(dan)
+      must_respond_with :not_found
+    end
+  end
 end

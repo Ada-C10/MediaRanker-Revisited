c05894ef80d175eb1655a113e77a74ea55f1a6e0
Anibel America <ameriabel@gmail.com>
2018-10-28 15:26:42 -0700

    adds tests for restricting user pages to guests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    if @login_user
+      @users = User.all
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Must be logged in to view page."
+      redirect_to root_path
+    end
   end
 
   def show
-    @user = User.find_by(id: params[:id])
-    render_404 unless @user
+    if @login_user
+      @user = User.find_by(id: params[:id])
+      render_404 unless @user
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Must be logged in to view page."
+      redirect_to root_path
+    end
   end
+
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,42 +2,99 @@ require 'test_helper'
 
 describe UsersController do
 
-  describe 'index' do
-    it 'succeeds when there are users' do
-      expect(User.count).must_be :>=, 1, "No users are set up in fixtures"
+  describe 'a logged in user' do
 
-      get users_path
-      must_respond_with :success
+    # before do
+    #   @user = users(:grace)
+    #   perform_login(@user)
+    #   @session = session[:user_id]
+    # end
+
+    describe 'index' do
+      it 'succeeds when there are users' do
+        user = users(:grace)
+        perform_login(user)
+
+        expect(User.count).must_be :>=, 1, "No users are set up in fixtures"
+
+        get users_path
+        must_respond_with :success
+      end
     end
 
-    it 'succeeds when there are no users' do
-      all_user_count = User.count
+    describe 'show' do
 
-      expect {
-        User.destroy_all
-      }.must_change('User.count', -all_user_count)
+      let(:existing_user) { User.last }
+
+      it 'succeeds for an existing user ID' do
+        user = users(:grace)
+        perform_login(user)
+
+        get user_path(existing_user)
+
+        must_respond_with :success
+      end
+
+      it 'renders 404 not found if user is not in the database' do
+        user = users(:grace)
+        perform_login(user)
+
+        id = existing_user.id
+        existing_user.destroy
+
+        get user_path(existing_user)
+
+        must_respond_with 404
+      end
+
     end
   end
 
-  describe 'show' do
+  describe 'a guest user' do
+    describe 'index' do
+      it 'redirects to the home page when there are users' do
+        expect(User.count).must_be :>=, 1, "No users are set up in fixtures"
 
-    let(:existing_user) { User.first }
+        get users_path
+        expect(flash[:status]).must_equal :failure
+        must_redirect_to root_path
+      end
 
-    it 'succeeds for an existing user ID' do
-      get user_path(existing_user)
+      it 'redirects to the home page when there are no users' do
+        all_user_count = User.count
 
-      must_respond_with :success
+        expect {
+          User.destroy_all
+        }.must_change('User.count', -all_user_count)
+
+        get users_path
+        expect(flash[:status]).must_equal :failure
+        must_redirect_to root_path
+      end
     end
 
-    it 'renders 404 not found if user is not in the database' do
-      id = existing_user.id
-      existing_user.destroy
+    describe 'show' do
 
-      get user_path(existing_user)
+      let(:existing_user) { User.first }
 
-      must_respond_with 404
+      it 'redirects to the root path when given an existing user ID' do
+        get user_path(existing_user)
+
+        must_redirect_to root_path
+        expect(flash[:status]).must_equal :failure
+      end
+
+      it 'redirects to the root path if user is not in the database' do
+        id = existing_user.id
+        existing_user.destroy
+
+        get user_path(existing_user)
+
+        must_redirect_to root_path
+        expect(flash[:status]).must_equal :failure
+      end
+
     end
-
   end
 
 end

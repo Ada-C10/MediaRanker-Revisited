d57bb967f64ad5d8362727ad2d2b1da4f79dab95
Daniela Di Iorio <danielad@gmail.com>
2018-10-29 13:38:42 -0700

    user controller tests accounting for authorization

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,27 +4,55 @@ describe UsersController do
 
   let(:dan) {users(:dan)}
 
-  describe 'index' do
-    it 'succeeds' do
-      get users_path
 
-      must_respond_with :success
+  describe 'logged-in user' do
+    describe 'index' do
+      it 'succeeds' do
+        perform_login(dan)
+        get users_path
+
+        must_respond_with :success
+      end
     end
 
+    describe 'show' do
+      it 'succeeds given a valid ID' do
+        perform_login(dan)
+        get user_path(dan.id)
+
+        must_respond_with :success
+      end
+
+      it 'responds with not found given an invalid ID' do
+        perform_login(dan)
+        get user_path(-1)
+
+        must_respond_with :not_found
+      end
+    end
   end
 
-  describe 'show' do
-    it 'succeeds given a valid ID' do
-      get user_path(dan.id)
+  describe 'guest user' do
+    describe 'index' do
+      it 'redirects to root path if no user is logged in' do
 
-      must_respond_with :success
+        get users_path
+
+        must_redirect_to root_path
+        expect(flash[:result_text]).must_equal "You must be logged in to view this section"
+      end
     end
 
-    it 'responds with not found given an invalid ID' do
-      get user_path(-1)
+    describe 'show' do
+      it 'redirects to root path if no user is logged in' do
 
-      must_respond_with :not_found
+        get user_path(dan.id)
+
+        must_redirect_to root_path
+        expect(flash[:result_text]).must_equal "You must be logged in to view this section"
+      end
     end
+
+
   end
-
 end

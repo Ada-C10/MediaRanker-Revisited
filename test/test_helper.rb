0b2a483ab71b20fcfc805e6b79c541b8156b2b7d
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-29 13:23:47 -0700

    Resolved merge conflict

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,20 +23,14 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   # Add more helper methods to be used by all tests here...
-<<<<<<< HEAD
-=======
 
->>>>>>> controller_tests
   def setup
     # Once you have enabled test mode, all requests
     # to OmniAuth will be short circuited to use the mock authentication hash.
     # A request to /auth/provider will redirect immediately to /auth/provider/callback.
     OmniAuth.config.test_mode = true
   end
-<<<<<<< HEAD
-  
-=======
->>>>>>> controller_tests
+
 end
 
 def mock_auth_hash(user)

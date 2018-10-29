ef762ff6c2c56370b33ee261a72e60d3fbb1de09
Daniela Di Iorio <danielad@gmail.com>
2018-10-29 13:29:53 -0700

    refactored vote model tests to use fixtures, section complete

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -16,29 +16,32 @@ describe Vote do
   end
 
   describe "validations" do
-    let (:user1) { User.new(username: 'chris') }
-    let (:user2) { User.new(username: 'chris') }
-    let (:work1) { Work.new(category: 'book', title: 'House of Leaves') }
-    let (:work2) { Work.new(category: 'book', title: 'For Whom the Bell Tolls') }
+    let (:kari) { users(:kari) }
+    let (:dan) { users(:dan)}
+    let (:album) { works(:album) }
+    let (:movie) { works(:movie) }
+    let (:poodr) { works(:poodr)}
 
     it "allows one user to vote for multiple works" do
-      vote1 = Vote.new(user: user1, work: work1)
+      vote1 = Vote.new(user: kari, work: poodr)
       vote1.save!
-      vote2 = Vote.new(user: user1, work: work2)
+      vote2 = Vote.new(user: kari, work: movie)
+
       vote2.valid?.must_equal true
     end
 
     it "allows multiple users to vote for a work" do
-      vote1 = Vote.new(user: user1, work: work1)
+      vote1 = Vote.new(user: kari, work: movie)
       vote1.save!
-      vote2 = Vote.new(user: user2, work: work1)
+      vote2 = Vote.new(user: dan, work: movie)
+
       vote2.valid?.must_equal true
     end
 
     it "doesn't allow the same user to vote for the same work twice" do
-      vote1 = Vote.new(user: user1, work: work1)
+      vote1 = Vote.new(user: kari, work: movie)
       vote1.save!
-      vote2 = Vote.new(user: user1, work: work1)
+      vote2 = Vote.new(user: kari, work: movie)
       vote2.valid?.must_equal false
       vote2.errors.messages.must_include :user
     end

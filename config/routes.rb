be7cb3d3798184006bfc2f5d3774e55b2f5e09b8
Addison Cole <addison.cole85@gmail.com>
2018-10-29 16:18:10 -0700

    changed link to method: :delete for logout link in application controller. messed around with routes and sessions but didnt really need to.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'works#root'
+
   get "/auth/:provider/callback", to: "sessions#login", as: "login"
-  post '/logout', to: 'sessions#logout', as: 'logout'
+  delete "/logout", to: "sessions#logout", as: "logout"
 
   resources :works
   post '/works/:id/upvote', to: 'works#upvote', as: 'upvote'

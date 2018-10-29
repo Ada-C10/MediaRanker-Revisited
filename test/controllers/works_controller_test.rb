23c23d0ae79fe0df0c42fdcf053a22305a19c7cb
Addison Cole <addison.cole85@gmail.com>
2018-10-29 12:50:47 -0700

    controller tests for works - new, index and root. first test for create complete.

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -10,11 +10,21 @@ describe WorksController do
 
     it "succeeds with one media type absent" do
       # Precondition: there is at least one media in two of the categories
+      work = Work.where(category: "movie")
+      delete work_path(work.ids)
 
+      get root_path
+      must_respond_with :success
     end
 
     it "succeeds with no media" do
+      works = Work.all
+      works.each do |work|
+        delete work_path(work.id)
+      end
 
+      get root_path
+      must_respond_with :success
     end
   end
 
@@ -23,23 +33,50 @@ describe WorksController do
 
   describe "index" do
     it "succeeds when there are works" do
-
+      get works_path
+      must_respond_with :success
     end
 
     it "succeeds when there are no works" do
+      works = Work.all
+      works.each do |work|
+        delete work_path(work.id)
+      end
 
+      get works_path
+      must_respond_with :success
     end
   end
 
   describe "new" do
     it "succeeds" do
-
+      get new_work_path
+      must_respond_with :success
     end
   end
 
   describe "create" do
     it "creates a work with valid data for a real category" do
 
+      CATEGORIES.each do |cat|
+
+        work_hash = {
+          work: {
+            title: "test title",
+            category: cat,
+          }
+        }
+
+        test_work = Work.new(work_hash[:work])
+
+        test_work.must_be :valid?, "Data was invalid. Please come fix this test"
+
+        expect {
+          post works_path, params: work_hash
+        }.must_change('Work.count', +1)
+        # Assert
+        must_redirect_to work_path(Work.last)
+      end
     end
 
     it "renders bad_request and does not update the DB for bogus data" do
@@ -51,67 +88,67 @@ describe WorksController do
     end
 
   end
-
-  describe "show" do
-    it "succeeds for an extant work ID" do
-
-    end
-
-    it "renders 404 not_found for a bogus work ID" do
-
-    end
-  end
-
-  describe "edit" do
-    it "succeeds for an extant work ID" do
-
-    end
-
-    it "renders 404 not_found for a bogus work ID" do
-
-    end
-  end
-
-  describe "update" do
-    it "succeeds for valid data and an extant work ID" do
-
-    end
-
-    it "renders bad_request for bogus data" do
-
-    end
-
-    it "renders 404 not_found for a bogus work ID" do
-
-    end
-  end
-
-  describe "destroy" do
-    it "succeeds for an extant work ID" do
-
-    end
-
-    it "renders 404 not_found and does not update the DB for a bogus work ID" do
-
-    end
-  end
-
-  describe "upvote" do
-
-    it "redirects to the work page if no user is logged in" do
-
-    end
-
-    it "redirects to the work page after the user has logged out" do
-
-    end
-
-    it "succeeds for a logged-in user and a fresh user-vote pair" do
-
-    end
-
-    it "redirects to the work page if the user has already voted for that work" do
-
-    end
-  end
+  #
+  # describe "show" do
+  #   it "succeeds for an extant work ID" do
+  #
+  #   end
+  #
+  #   it "renders 404 not_found for a bogus work ID" do
+  #
+  #   end
+  # end
+  #
+  # describe "edit" do
+  #   it "succeeds for an extant work ID" do
+  #
+  #   end
+  #
+  #   it "renders 404 not_found for a bogus work ID" do
+  #
+  #   end
+  # end
+  #
+  # describe "update" do
+  #   it "succeeds for valid data and an extant work ID" do
+  #
+  #   end
+  #
+  #   it "renders bad_request for bogus data" do
+  #
+  #   end
+  #
+  #   it "renders 404 not_found for a bogus work ID" do
+  #
+  #   end
+  # end
+  #
+  # describe "destroy" do
+  #   it "succeeds for an extant work ID" do
+  #
+  #   end
+  #
+  #   it "renders 404 not_found and does not update the DB for a bogus work ID" do
+  #
+  #   end
+  # end
+  #
+  # describe "upvote" do
+  #
+  #   it "redirects to the work page if no user is logged in" do
+  #
+  #   end
+  #
+  #   it "redirects to the work page after the user has logged out" do
+  #
+  #   end
+  #
+  #   it "succeeds for a logged-in user and a fresh user-vote pair" do
+  #
+  #   end
+  #
+  #   it "redirects to the work page if the user has already voted for that work" do
+  #
+  #   end
+  # end
 end

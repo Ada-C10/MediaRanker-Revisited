1d0650b4fbd51068c8ab7c4a30624225920b0431
Addison Cole <addison.cole85@gmail.com>
2018-10-29 16:03:51 -0700

    create controller action for login, create helper method to build a user hash from github, changed login route to github auth, and create migration to add columns to users to store new github login info.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,26 +2,48 @@ class SessionsController < ApplicationController
   def login_form
   end
 
+  # def login
+  #   username = params[:username]
+  #   if username and user = User.find_by(username: username)
+  #     session[:user_id] = user.id
+  #     flash[:status] = :success
+  #     flash[:result_text] = "Successfully logged in as existing user #{user.username}"
+  #   else
+  #     user = User.new(username: username)
+  #     if user.save
+  #       session[:user_id] = user.id
+  #       flash[:status] = :success
+  #       flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+  #     else
+  #       flash.now[:status] = :failure
+  #       flash.now[:result_text] = "Could not log in"
+  #       flash.now[:messages] = user.errors.messages
+  #       render "login_form", status: :bad_request
+  #       return
+  #     end
+  #   end
+  #   redirect_to root_path
+  # end
+
   def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
-      session[:user_id] = user.id
-      flash[:status] = :success
-      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
+    auth_hash = request.env['omniauth.auth']
+
+    user = User.find_by(uid: auth_hash[:uid], provider: 'github')
+    if user
+      flash[:success] = "Logged in as returning user #{user.name}"
     else
-      user = User.new(username: username)
+      user = User.build_hash_from_github(auth_hash)
+
       if user.save
-        session[:user_id] = user.id
-        flash[:status] = :success
-        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+        flash[:success] = "Logged in as new user #{user.name}"
       else
-        flash.now[:status] = :failure
-        flash.now[:result_text] = "Could not log in"
-        flash.now[:messages] = user.errors.messages
-        render "login_form", status: :bad_request
+        flash[:error] = "Could not create new user account: #{user.errors.messages}"
+        redirect_to root_path
         return
       end
     end
+
+    session[:user_id] = user.id
     redirect_to root_path
   end
 

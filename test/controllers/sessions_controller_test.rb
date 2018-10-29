5de51edc3a7f4d793a03df200d8eb93120f9305a
Katrina Aganon <katrina.aganon@gmail.com>
2018-10-28 18:04:28 -0700

    update controller tests for works and sessions

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,5 +1,45 @@
 require "test_helper"
 
 describe SessionsController do
+  it 'can login with github as an existing user and redirects to root route' do
+
+    start_count = User.count
+
+    dan = users(:dan)
+
+    perform_login(dan)
+
+    must_redirect_to root_path
+    expect(session[:user_id]).must_equal dan.id
+    expect(User.count).must_equal start_count
+
+  end
+
+  it 'can create a new user and login with github, and redirects to root route' do
+    start_count = User.count
+
+    user = User.new(provider: 'github', uid: 123, name: "Beep boop", email: "beepboop@newmail.com")
+
+    perform_login(user)
+
+    must_redirect_to root_path
+    expect(User.count).must_equal start_count + 1
+    expect(session[:user_id]).must_equal User.last.id
+  end
+
+  it 'it does not create a new user if given invalid user data and redirects to root route' do
+    start_count = User.count
+
+    invalid_user = User.new(name: nil, email: '123@email.com')
+
+    expect(invalid_user).must_be :invalid?
+
+    perform_login(invalid_user)
+
+    must_redirect_to root_path
+    expect(session[:user_id]).must_equal nil
+    expect(User.count).must_equal start_count
+  end
+
 
 end
--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'pry'
 
 describe WorksController do
 
@@ -29,8 +30,8 @@ describe WorksController do
       get root_path
 
       must_respond_with :success
-
     end
+
   end
 
   CATEGORIES = %w(album book movie) # changed categories to singular
@@ -89,6 +90,25 @@ describe WorksController do
 
     it "renders bad_request and does not update the DB for bogus data" do
 
+        work = Work.new(category: 'album', title: '')
+
+        work.must_be :invalid?, "Work data wasn't invalid, please fix me."
+
+      work_data = {
+        work: {
+          title: '',
+          category: CATEGORIES.first
+        }
+      }
+
+      expect {
+        post works_path, params: work_data
+      }.wont_change('Work.count')
+
+      must_respond_with :bad_request
+    end
+
+    it "renders 400 bad_request for bogus categories" do
       INVALID_CATEGORIES.each do |c|
         work = Work.new(category: c, title: 'a title')
 
@@ -109,10 +129,6 @@ describe WorksController do
       must_respond_with :bad_request
     end
 
-    it "renders 400 bad_request for bogus categories" do
-      skip
-    end
-
   end
 
   describe "show" do
@@ -125,60 +141,194 @@ describe WorksController do
     end
 
     it "renders 404 not_found for a bogus work ID" do
-      skip
+      bogus_work_id = Work.last.id + 1
+
+      get work_path(bogus_work_id)
+
+      must_respond_with :not_found
     end
   end
 
   describe "edit" do
     it "succeeds for an extant work ID" do
-      skip
+      existing_work = works(:poodr)
+
+      get work_path(existing_work.id)
+
+      must_respond_with :success
     end
 
     it "renders 404 not_found for a bogus work ID" do
-      skip
+      bogus_work_id = works(:poodr).id
+      works(:poodr).destroy
+
+      get edit_work_path(bogus_work_id)
+
+      must_respond_with :not_found
     end
   end
 
   describe "update" do
+    let (:work_hash) {
+      {
+        work: {
+          title: 'A brand new title',
+          category: 'book'
+        }
+      }
+    }
+
     it "succeeds for valid data and an extant work ID" do
-      skip
+      work_id = works(:movie).id
+
+      expect {
+        patch work_path(work_id), params: work_hash
+      }.wont_change('Work.count')
+
+      must_respond_with :redirect
+
+      work = Work.find_by(id: work_id)
+      expect(work.title).must_equal work_hash[:work][:title]
+      expect(work.category).must_equal work_hash[:work][:category]
+
     end
 
     it "renders bad_request for bogus data" do
-      skip
+      original_work = works(:album)
+      work_id = works(:album).id
+
+      work_hash[:work][:category] = INVALID_CATEGORIES.last
+
+      expect {
+        patch work_path(work_id), params: work_hash
+      }.wont_change('Work.count')
+
+      must_respond_with :bad_request
+
+      work = Work.find_by(id: work_id)
+      expect(work.title).must_equal original_work.title
+      expect(work.category).must_equal original_work.category
     end
 
     it "renders 404 not_found for a bogus work ID" do
-      skip
+      id = Work.last.id + 1
+
+      expect{
+        patch work_path(id), params: work_hash
+      }.wont_change('Work.count')
+
+      must_respond_with :not_found
     end
   end
 
   describe "destroy" do
     it "succeeds for an extant work ID" do
-      skip
+      work_id = works(:another_album).id
+
+      expect{
+        delete work_path(work_id)
+      }.must_change('Work.count', -1)
+
+      must_redirect_to root_path
     end
 
     it "renders 404 not_found and does not update the DB for a bogus work ID" do
-      skip
+      id = Work.last.id + 1
+
+      expect{
+        delete work_path(id)
+      }.wont_change('Work.count')
+
+      must_respond_with :not_found
     end
   end
 
   describe "upvote" do
-
     it "redirects to the work page if no user is logged in" do
-      skip
+      work_id = works(:poodr).id
+      start_count = Vote.count
+
+      expect {
+        post upvote_path(work_id)
+      }.wont_change('Vote.count')
+
+      must_redirect_to work_path(work_id)
+      flash[:status].must_equal :failure
+      flash[:result_text].must_equal "You must log in to do that"
+      expect(Vote.count).must_equal start_count
     end
 
     it "redirects to the work page after the user has logged out" do
-      skip
+      user = users(:dan)
+      work_id = works(:poodr).id
+      perform_login(user)
+      start_count = Vote.count
+
+      delete logout_path
+
+      expect {
+        post upvote_path(work_id)
+      }.wont_change('Vote.count')
+
+
+      expect(session[:user_id]).must_equal nil
+      flash[:status].must_equal :failure
+      must_redirect_to work_path(work_id)
+      expect(Vote.count).must_equal start_count
     end
 
     it "succeeds for a logged-in user and a fresh user-vote pair" do
-      skip
+
+      # First user vote
+      user_1 = users(:dan)
+      work_id = works(:movie).id
+      start_count = Vote.count
+
+      perform_login(user_1)
+
+      expect {
+        post upvote_path(work_id)
+      }.must_change('Vote.count', +1)
+
+      flash[:status].must_equal :success
+      flash[:result_text].must_equal "Successfully upvoted!"
+      must_redirect_to work_path(work_id)
+      expect(Vote.count).must_equal start_count + 1
+      expect(Vote.last.user.id).must_equal user_1.id
+      must_redirect_to work_path(work_id)
+
+      # Second user vote
+      user_2 = users(:kari)
+      perform_login(user_2)
+
+      expect {
+        post upvote_path(work_id)
+      }.must_change('Vote.count', +1)
+
+      flash[:status].must_equal :success
+      flash[:result_text].must_equal "Successfully upvoted!"
+      must_redirect_to work_path(work_id)
+      expect(Vote.count).must_equal start_count + 2
+      expect(Vote.last.user.id).must_equal user_2.id
+      must_redirect_to work_path(work_id)
     end
 
     it "redirects to the work page if the user has already voted for that work" do
-      skip
+      work_id = works(:poodr).id
+      user = users(:kari)
+      perform_login(user)
+
+      post upvote_path(work_id)
+
+      flash[:status].must_equal :success
+      flash[:result_text].must_equal "Successfully upvoted!"
+
+      post upvote_path(work_id)
+
+      flash[:status].must_equal :failure
+      flash[:result_text].must_equal "Could not upvote"
+      flash[:messages].must_include :user
+      must_redirect_to work_path(work_id)
     end
   end
 

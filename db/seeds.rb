9c762c5b6552488d71945df75960895d8a7c548b
jackie <jacquelyn.cheng@me.com>
2018-10-29 15:52:25 -0700

    Modified seed file to account for work ownership by user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 require 'csv'
 media_file = Rails.root.join('db', 'media_seeds.csv')
 
+User.create(provider: "github", uid: 99999, username: "seed_user", name: "Seed Person")
+
 CSV.foreach(media_file, headers: true, header_converters: :symbol, converters: :all) do |row|
   data = Hash[row.headers.zip(row.fields)]
   puts data

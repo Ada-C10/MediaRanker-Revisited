18939999c6c894be8335e1a8a7fb1a876bbb7b81
Valerie Gidzinski <valgidzi@gmail.com>
2018-10-30 06:28:27 -0700

    Add UsersController#show test, add login method to all tests, fix test data

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,42 +1,31 @@
 require 'test_helper'
 
 describe UsersController do
-  let (:dan) {users(:dan)}
-  let (:kari) {users(:kari)}
+  describe 'logged in users' do
+    let (:dan) {users(:dan)}
+    let (:kari) {users(:kari)}
 
-  before do
-    Vote.all.each do |vote|
-      vote.destroy
-    end
-  end
-
-  describe 'index' do
-    it 'succeeds when there are users' do
-      get users_path
-      must_respond_with :success
+    describe 'index' do
+      it 'succeeds when there are users' do
+        perform_login(dan)
+        get users_path
+        must_respond_with :success
+      end
     end
 
-    it 'succeeds when there are no users' do
 
-      dan.destroy
-      kari.destroy
+    describe 'show' do
+      it 'succeeds when user exists' do
+        perform_login(dan)
+        get user_path(kari)
+        must_respond_with :success
+      end
 
-      get users_path
-      must_respond_with :success
-    end
-  end
-
-  describe 'show' do
-    it 'succeeds when user exists' do
-      get user_path(dan)
-      must_respond_with :success
-    end
-
-    it 'renders 404 if user does not exist' do
-      dan.destroy
-
-      get user_path(dan)
-      must_respond_with :not_found
+      it 'renders 404 if user does not exist' do
+        perform_login(dan)
+        get user_path(User.last.id + 1)
+        must_respond_with :not_found
+      end
     end
   end
 end

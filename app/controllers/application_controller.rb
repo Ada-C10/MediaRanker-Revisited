a10574b07166f4c3a3ba2ab524b12688088525c2
Valerie Gidzinski <valgidzi@gmail.com>
2018-10-30 06:08:43 -0700

    Add require_login method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,25 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :find_user
+    before_action :require_login, except: [:root]
 
   def render_404
     # DPR: this will actually render a 404 page in production
     raise ActionController::RoutingError.new('Not Found')
   end
 
+
 private
   def find_user
     if session[:user_id]
       @login_user = User.find_by(id: session[:user_id])
+    end
+  end
+  def require_login
+    if @login_user.nil?
+      flash[:status] = :failure
+      flash[:result_text] = "You must be logged in to do that."
+      redirect_back(fallback_location: root_path)
     end
   end
 end

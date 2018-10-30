ceaa573ac003811a4b4ec6a6f0bde7fb8cb15a59
Danielle Metzner <danimetzner@gmail.com>
2018-10-29 23:14:59 -0700

    Fixed tests for user and vote models

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,12 +1,9 @@
 require 'test_helper'
 
 describe User do
-  before do
-    perform_login(users(:grace)
-  end
+  let(:grace) {users(:grace)}
   describe "relations" do
     it "has a list of votes" do
-      grace = users(:grace)
       grace.must_respond_to :votes
       grace.votes.each do |vote|
         vote.must_be_kind_of Vote
@@ -14,7 +11,6 @@ describe User do
     end
 
     it "has a list of ranked works" do
-      grace = users(:grace)
       grace.must_respond_to :ranked_works
       grace.ranked_works.each do |work|
         work.must_be_kind_of Work
@@ -31,12 +27,12 @@ describe User do
 
     it "requires a unique username" do
       username = "test username"
-      user1 = User.new(username: username)
+      user1 = User.new(username: username, uid:1, provider: "github")
 
       # This must go through, so we use create!
       user1.save!
 
-      user2 = User.new(username: username)
+      user2 = User.new(username: username, uid:123, provider: "github")
       result = user2.save
       result.must_equal false
       user2.errors.messages.must_include :username

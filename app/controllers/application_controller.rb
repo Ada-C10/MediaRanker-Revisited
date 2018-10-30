e08d97d4e3ca6907489c36c395da4c6509c9bce2
kangazoom <michellelcronin@gmail.com>
2018-10-29 23:36:50 -0700

    user controller tests pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :find_user
+  before_action :find_user, except: [:root]
 
   def render_404
     # DPR: this will actually render a 404 page in production
@@ -12,6 +12,15 @@ private
   def find_user
     if session[:user_id]
       @login_user = User.find_by(id: session[:user_id])
+    end
+  end
+
+  def must_login
+    if @login_user.nil?
+      flash[:status] = :failure
+      flash[:result_test] = 'You must log in to do that'
+
+      redirect_to root_path
     end
   end
 end

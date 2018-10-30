1ec7116581d235280c461013545c91efd3244486
Divya <divyamohanan@gmail.com>
2018-10-29 22:01:19 -0700

    Able to login with Github

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,24 @@ class User < ApplicationRecord
   has_many :votes
   has_many :ranked_works, through: :votes, source: :work
 
-  def self.github(auth_hash)
-   user = User.new
-   user.uid = auth_hash[:uid]
-   user.provider = 'github'
-   user.name = auth_hash['info']['name']
-   user.email = auth_hash['info']['email']
-
-   # Note that the user has not been saved
-   return user
+  # def self.build_from_github(auth_hash)
+  #  user = User.new
+  #  user.uid = auth_hash[:uid]
+  #  user.provider = 'github'
+  #  user.name = auth_hash['info']['name']
+  #  user.email = auth_hash['info']['email']
+  #
+  #  # Note that the user has not been saved
+  #  return user
+  # end
+  def self.create_from_github(auth_hash)
+     user = User.new
+     user.uid = auth_hash[:uid]
+     user.provider = auth_hash[:provider]
+     user.username = auth_hash['info']['name']
+      # Note that the user has not been saved
+     return user
   end
-
 
   validates :username, uniqueness: true, presence: true
 end

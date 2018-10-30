c5b80098f75981e35f559ed76341cbdbf3b1c609
Karis Kim <ouniiq@hotmail.com>
2018-10-30 09:45:07 -0700

    working on sessions tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,58 +1,15 @@
 class SessionsController < ApplicationController
 
- #  def create
- #    auth_hash = request.env['omniauth.auth']
- #
- #    user = User.find_by(uid: auth_hash[:uid], provider: 'github')
- #    # require 'pry'; binding.pry
- #    if user
- #      # User was found in the database
- #      flash[:status] = :success
- #      flash[:result_text] = "Logged in as returning user #{user.name}"
- #
- #
- #    else
- #      # User doesn't match anything in the DB
- #      # Attempt to create a new user
- #      user = User.build_from_github(auth_hash)
- #
- #      if user.save
- #        flash[:status] = :success
- #        flash[:result_text] = "Logged in as returning user #{user.name}"
- #
- #      else
- #        # Couldn't save the user for some reason. If we
- #        # hit this it probably means there's a bug with the
- #        # way we've configured GitHub. Our strategy will
- #        # be to display error messages to make future
- #        # debugging easier.
- #        flash[:status] = :error
- #        flash[:result_text] = "Could not create new user account: #{user.errors.messages}"
- #        redirect_to root_path
- #        return
- #      end
- #    end
- #
- #    # If we get here, we have a valid user instance
- #    session[:user_id] = user.id
- #    redirect_to root_path
- #  end
- #
- #  def destroy
- #   session[:user_id] = nil
- #   flash[:status] = :success
- #   flash[:result_text] = "Successfully logged out!"
- #
- #   redirect_to root_path
- # end
-
   def login_form
   end
 
   def login
-    username = params[:username]
+    auth_hash = request.env['omniauth.auth']
+    #
+    user = User.find_by(uid: auth_hash[:uid], provider: 'github')
+    # username = params[:username]
 # require 'pry'; binding.pry
-    if username and user = User.find_by(username: username)
+    if user
       session[:user_id] = user.id
       flash[:status] = :success
       flash[:result_text] = "Successfully logged in as existing user #{user.username}"
@@ -80,3 +37,49 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 end
+
+#  def create
+#    auth_hash = request.env['omniauth.auth']
+#
+#    user = User.find_by(uid: auth_hash[:uid], provider: 'github')
+#    # require 'pry'; binding.pry
+#    if user
+#      # User was found in the database
+#      flash[:status] = :success
+#      flash[:result_text] = "Logged in as returning user #{user.name}"
+#
+#
+#    else
+#      # User doesn't match anything in the DB
+#      # Attempt to create a new user
+#      user = User.build_from_github(auth_hash)
+#
+#      if user.save
+#        flash[:status] = :success
+#        flash[:result_text] = "Logged in as returning user #{user.name}"
+#
+#      else
+#        # Couldn't save the user for some reason. If we
+#        # hit this it probably means there's a bug with the
+#        # way we've configured GitHub. Our strategy will
+#        # be to display error messages to make future
+#        # debugging easier.
+#        flash[:status] = :error
+#        flash[:result_text] = "Could not create new user account: #{user.errors.messages}"
+#        redirect_to root_path
+#        return
+#      end
+#    end
+#
+#    # If we get here, we have a valid user instance
+#    session[:user_id] = user.id
+#    redirect_to root_path
+#  end
+#
+#  def destroy
+#   session[:user_id] = nil
+#   flash[:status] = :success
+#   flash[:result_text] = "Successfully logged out!"
+#
+#   redirect_to root_path
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,7 @@ Rails.application.routes.draw do
   post '/works/:id/upvote', to: 'works#upvote', as: 'upvote'
 
   resources :users, only: [:index, :show]
-# get "/auth/:provider/callback", to: "sessions#login", as: "logging_in"
-  get "/auth/:provider/callback", to: "sessions#login", as: "login"
 
+  get "/auth/:provider/callback", to: "sessions#login", as: "login"
   delete "/logout", to: "sessions#logout", as: "logout"
 end

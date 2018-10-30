b15a2345ba7c1af30d5e322beafffadaea4a4ff7
Divya <divyamohanan@gmail.com>
2018-10-29 17:10:04 -0700

    Tests for some methods in works controller

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,5 +1,136 @@
+# require "test_helper"
+#
+# describe SessionsController do
+#   it "login form" do
+#     get login_path
+#
+#     must_respond_with :success
+#   end
+#
+#   describe "login action" do
+#     it "can create a new user" do
+#       user_hash = {
+#         author: {
+#           name: 'katie'
+#         }
+#       }
+#       expect {
+#         post login_path, params: user_hash
+#       }.must_change 'User.count', 1
+#
+#       must_respond_with :redirect
+#       must_redirect_to root_path
+#
+#       new_user = User.find_by(name: user_hash[:author][:name])
+#       expect(new_user).wont_be_nil
+#       expect(session[:user_id]).must_equal new_user.id
+#     end
+#     it "should log in an existing user without changing the DB" do
+#
+#     end
+#
+#     it "should give a bad_request for an invalid user name" do
+#
+#     end
+#
+#
+#   end
+#
+#   it 'Sample' do
+#     people = 47
+#
+#     expect {
+#       people += 5
+#     }.must_change 'people', 5
+#
+#
+#   end
+#
+# end
+
 require "test_helper"
 
 describe SessionsController do
+
+	describe "auth_callback" do # aka login
+	 it "logs in an existing user and redirects to the root route" do
+			start_count = User.count
+			user = users(:dan)
+
+			login_for_test(user)
+			must_redirect_to root_path
+			session[:user_id].must_equal user.id
+
+			User.count.must_equal start_count
+
+		 ## longer way before shortcut
+		 # start_count = User.count
+		 # user = users(:dan)
+		 #
+		 # OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+		 # get auth_callback_path(:github)
+		 #
+		 # must_redirect_to root_path
+		 # session[:user_id].must_equal user.id
+		 # User.count.must_equal start_count
+	 end
+
+	 it "creates an account for a new user and redirects to the root route" do
+			start_count = User.count
+			user = User.new(provider: "github", uid: 99999, username: "test_user",
+				email: "test@user.com")
+
+			OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+			get auth_callback_path(:github)
+
+			must_redirect_to root_path
+
+			User.count.must_equal start_count + 1
+
+			session[:user_id].must_equal User.last.id
+	 end
+
+	 it "redirects to the login route if given invalid user data" do
+		 start_count = User.count
+		 user = User.new(provider: "github", uid: 99999, username: nil,
+			 email: "test@user.com") # username is nil
+
+		 OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+		 get auth_callback_path(:github)
+
+		 must_redirect_to root_path
+
+		 User.count.must_equal start_count
+
+		 session[:user_id].must_be_nil
+	 end
+ end
+
+	describe "auth_callback" do # aka login
+		it "logs in an existing user and redirects to the root route" do
+			start_count = User.count
+			user = users(:dan)
+
+			login_for_test(user)
+			must_redirect_to root_path
+			session[:user_id].must_equal user.id
+
+			User.count.must_equal start_count
+
+		end
+	end
+
+	describe "destroy" do
+		it "destroys the session" do
+			user = users(:dan)
+			login_for_test(user)
+			session[:user_id].must_equal user.id # just checking to be sure
+
+			logout_for_test(user)
+
+			must_redirect_to root_path
+			session[:user_id].must_be_nil
+		end
+	end
 
 end
--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -4,17 +4,29 @@ describe WorksController do
   describe "root" do
     it "succeeds with all media types" do
       # Precondition: there is at least one media of each category
+      get root_path
+      must_respond_with :success
 
     end
 
     it "succeeds with one media type absent" do
       # Precondition: there is at least one media in two of the categories
 
+      Work.find_by(category: "movie").destroy
+       get root_path
+       expect(Work.find_by(category: "movie")).must_be_nil
+      must_respond_with :success
     end
 
     it "succeeds with no media" do
-
+      Work.all.each do |work|
+        work.destroy
+      end
+       get root_path
+       expect(Work.all.length).must_equal 0
+      must_respond_with :success
     end
+
   end
 
   CATEGORIES = %w(albums books movies)
@@ -22,30 +34,68 @@ describe WorksController do
 
   describe "index" do
     it "succeeds when there are works" do
+      get works_path
+       must_respond_with :success
 
     end
 
     it "succeeds when there are no works" do
-
+      Work.all.each do |work|
+        work.destroy
+      end
+       get works_path
+       must_respond_with :success
     end
   end
 
   describe "new" do
     it "succeeds" do
-
+      get new_work_path
+      must_respond_with :success
     end
   end
 
   describe "create" do
+    let (:media_hash) {
+      {
+        work: {
+          title: "Bee Movie",
+          creator: "Walt Disney",
+          description: "Cute!!",
+          publication_year: 2014,
+          category: "movie"
+        }
+      }
+    }
     it "creates a work with valid data for a real category" do
+      expect {
+        post works_path, params: media_hash
+      }.must_change 'Work.count', 1
+       new_media = Work.last
+       must_respond_with :redirect
+      must_redirect_to work_path(new_media.id)
+      expect(new_media.title).must_equal  media_hash[:work][:title]
+      expect(new_media.creator).must_equal  media_hash[:work][:creator]
+      expect(new_media.description).must_equal  media_hash[:work][:description]
+      expect(new_media.publication_year).must_equal  media_hash[:work][:publication_year]
+      expect(new_media.category).must_equal  media_hash[:work][:category]
 
     end
 
     it "renders bad_request and does not update the DB for bogus data" do
-
+      media_hash[:work][:title] = nil
+       expect {
+        post works_path, params: media_hash
+      }.wont_change 'Work.count'
+       must_respond_with :bad_request
     end
 
     it "renders 400 bad_request for bogus categories" do
+      media_hash[:work][:category] = "bogus"
+       expect {
+        post works_path, params: media_hash
+      }.wont_change 'Work.count'
+       must_respond_with :bad_request
 
     end
 
@@ -53,35 +103,82 @@ describe WorksController do
 
   describe "show" do
     it "succeeds for an extant work ID" do
-
+      id = works(:album).id
+      get work_path(id)
+      must_respond_with :success
     end
 
     it "renders 404 not_found for a bogus work ID" do
-
+      id = -1
+       get work_path(id)
+       must_respond_with :not_found
     end
   end
 
   describe "edit" do
     it "succeeds for an extant work ID" do
-
+      id = works(:album).id
+       get edit_work_path(id)
+       must_respond_with :success
     end
 
     it "renders 404 not_found for a bogus work ID" do
-
+      id = -1
+       get work_path(id)
+       must_respond_with :not_found
     end
   end
 
   describe "update" do
+    let (:media_hash) {
+      {
+        work: {
+          title: "Bee Movie",
+          creator: "Walt Disney",
+          description: "Cute!!",
+          publication_year: 2014,
+          category: "movie"
+        }
+      }
+    }
     it "succeeds for valid data and an extant work ID" do
+      id = works(:movie).id
+       expect {
+        patch work_path(id), params: media_hash
+      }.wont_change 'Work.count'
+       updated_media = Work.find(id)
+       must_respond_with :redirect
+      must_redirect_to work_path(id)
+      expect(updated_media.title).must_equal media_hash[:work][:title]
+      expect(updated_media.creator).must_equal media_hash[:work][:creator]
+      expect(updated_media.description).must_equal media_hash[:work][:description]
+      expect(updated_media.publication_year).must_equal media_hash[:work][:publication_year]
+      expect(updated_media.category).must_equal media_hash[:work][:category]
 
     end
 
     it "renders bad_request for bogus data" do
-
+      media_hash[:work][:category] = nil
+       old_media = works(:movie)
+      id = old_media.id
+       expect {
+        patch work_path(id), params: media_hash
+      }.wont_change 'Work.count'
+       new_media = Work.find(id)
+       must_respond_with :not_found
+      expect(old_media.title).must_equal new_media.title
+      expect(old_media.creator).must_equal new_media.creator
+      expect(old_media.description).must_equal new_media.description
+      expect(old_media.publication_year).must_equal new_media.publication_year
+      expect(old_media.category).must_equal new_media.category
     end
 
     it "renders 404 not_found for a bogus work ID" do
-
+      id = -1
+       expect {
+        patch work_path(id), params: media_hash
+      }.wont_change 'Work.count'
+       must_respond_with :not_found
     end
   end
 

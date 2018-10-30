1c19d409c5e96775b97665256113cabdf9b3bef0
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-29 17:17:19 -0700

    user controller tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,6 @@ class ApplicationController < ActionController::Base
 
   private
 
-
-
   def find_user
     if session[:user_id]
       @login_user = User.find_by(id: session[:user_id])
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,4 +2,54 @@ require 'test_helper'
 
 describe UsersController do
 
+  before do
+    @dan = users(:dan)
+    @kari = users(:kari)
+  end
+
+  let (:logged_in_user) {
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(@dan))
+
+    get auth_callback_path(:github)
+  }
+
+  describe 'index' do
+    it 'redirects home for guest user' do
+
+      get works_path
+      must_redirect_to root_path
+    end
+
+    it 'succeeds for a loggedin user' do
+      logged_in_user
+
+      get works_path
+      must_respond_with :success
+    end
+  end
+
+  describe "show" do
+
+    it 'does not succeed for a guest user' do
+
+      get user_path(@dan)
+      must_redirect_to root_path
+    end
+
+    it "succeeds for an extant user ID" do
+
+      logged_in_user
+
+      get user_path(@kari)
+      must_respond_with :success
+    end
+
+    it "renders 404 not_found for a bogus user ID" do
+
+      logged_in_user
+
+      get user_path(0)
+      must_respond_with :not_found
+    end
+  end
 end

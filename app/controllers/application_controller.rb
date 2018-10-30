f9e591a1383979bd780ddfff6ba0c844758dfaf4
Lindsay Terchin <lterchin@gmail.com>
2018-10-29 17:34:39 -0700

    added require login private method to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :find_user
+  before_action :require_login
 
   def render_404
     # DPR: this will actually render a 404 page in production
@@ -9,9 +10,16 @@ class ApplicationController < ActionController::Base
   end
 
 private
-  def find_user
-    if session[:user_id]
-      @login_user = User.find_by(id: session[:user_id])
+  def find_logged_in_user
+      @logged_in_user = User.find_by(id: session[:user_id])
     end
+  end
+
+  def require_login
+    if @logged_in_user.nil?
+      flash[:error] = "You must be logged in to do this."
+      redirect_to root_path
+    end
+
   end
 end

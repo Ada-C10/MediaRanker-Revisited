e558b01606115b507d0c97960fc13c5fe10a6e03
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-29 19:41:56 -0700

    Changed users/index.html.erb voting loop

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,5 +1,15 @@
 require 'test_helper'
 
 describe UsersController do
+  describe 'index' do
 
+    let(:person){users(:june)}
+
+    it 'a logged in user can access the view template associated with Users#index' do
+      perform_login(person)
+      get users_path
+      must_respond_with :success
+    end
+
+  end
 end

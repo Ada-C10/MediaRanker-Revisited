6abb74a151e1e90ea949a7671a852b64d2ae886a
jackie <jacquelyn.cheng@me.com>
2018-10-29 22:07:19 -0700

    Added Google OAuth login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,16 @@ class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
 
-    user = User.find_by(uid: auth_hash[:uid], provider: 'github')
+    user = User.find_by(uid: auth_hash[:uid], provider: auth_hash[:provider])
+
     if user
       session[:user_id] = user.id
       flash[:status] = :success
       flash[:result_text] = "Successfully logged in as existing user #{user.username}"
     else
-      user = User.build_from_github(auth_hash)
-
+      user = User.build_from_github(auth_hash) if auth_hash[:provider] == 'github'
+      user = User.build_from_google(auth_hash) if auth_hash[:provider] == 'google_oauth2'
+      
       if user.save
         session[:user_id] = user.id
         flash[:status] = :success

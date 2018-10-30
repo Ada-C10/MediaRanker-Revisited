8286bfbfe48a62dc705b078328a0f52a695cd019
Layla Bristol <layla.bristol@gmail.com>
2018-10-29 22:57:56 -0700

    added tests for initial crud actions

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -4,15 +4,30 @@ describe WorksController do
   describe "root" do
     it "succeeds with all media types" do
       # Precondition: there is at least one media of each category
+      get root_path
+      must_respond_with :success
 
     end
 
     it "succeeds with one media type absent" do
       # Precondition: there is at least one media in two of the categories
+      work = Work.find_by(category: "movie")
+
+      work.destroy
+
+      get root_path
+      must_respond_with :success
 
     end
 
     it "succeeds with no media" do
+
+      works.each do |work|
+        work.destroy
+      end
+
+      get root_path
+      must_respond_with :success
 
     end
   end
@@ -22,37 +37,120 @@ describe WorksController do
 
   describe "index" do
     it "succeeds when there are works" do
+      work_id = Work.first.id
+
+      get work_path(work_id)
+      must_respond_with :success
 
     end
 
     it "succeeds when there are no works" do
 
+      Work.all.each { |work| work.destroy }
+
+      Work.count.must_equal 0
+
+      get works_path
+      must_respond_with :success
+
     end
   end
 
   describe "new" do
+
+    before do
+      login_for_test(users(:dee))
+    end
+
     it "succeeds" do
+
+      get new_work_path
+      must_respond_with :success
 
     end
   end
 
   describe "create" do
+
+    before do
+        login_for_test(users(:dee))
+      end
+
     it "creates a work with valid data for a real category" do
+
+      test_hash = {
+             work: {
+               title: "test-title",
+               creator: "test-creator",
+               description: "test-description",
+               publication_year: 2020,
+               category: "movie"
+             }
+           }
+
+
+        expect {
+        post works_path, params: test_hash
+        }.must_change 'Work.count', 1
+
+        work = Work.find_by(title: test_hash[:work][:title])
+
+        expect(work.creator).must_equal test_hash[:work][:creator]
+        expect(work.description).must_equal test_hash[:work][:description]
+        expect(work.publication_year).must_equal test_hash[:work][:publication_year]
+        expect(work.category).must_equal test_hash[:work][:category]
+        must_redirect_to work_path(work)
 
     end
 
     it "renders bad_request and does not update the DB for bogus data" do
 
+      bad_hash = {
+        work: {
+          title: "test-title",
+          creator: "test-creator",
+          description: "test-description",
+          publication_year: 2020,
+          category: "movie"
+        }
+      }
+
+      expect {
+        post works_path, params: bad_hash
+      }.wont_change "Work.count"
+
+      must_respond_with :bad_request
+
     end
 
     it "renders 400 bad_request for bogus categories" do
 
+      work_hash = {
+        work: {
+          title: "test-title",
+          creator: "test-creator",
+          description: "test-description",
+          publication_year: 2020,
+          category: "fruit"
+        }
+      }
+
+      expect {
+      post works_path, params: bad_hash
+      }.wont_change 'Work.count'
+
+      must_respond_with :bad_request
     end
 
   end
 
   describe "show" do
     it "succeeds for an extant work ID" do
+
+      work = Work.last.id + 1
+      get work_path(work)
+      must_respond_with :not_found
+
 
     end
 

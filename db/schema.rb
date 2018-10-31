03c60edfbddfe92f1f5060066c012efceb83494d
Divya <divyamohanan@gmail.com>
2018-10-31 00:03:14 -0700

    Migrations to remove unnecessary associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_040545) do
+ActiveRecord::Schema.define(version: 2018_10_31_063529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,11 +42,8 @@ ActiveRecord::Schema.define(version: 2018_10_30_040545) do
     t.datetime "updated_at", null: false
     t.integer "vote_count", default: 0
     t.integer "publication_year"
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_works_on_user_id"
   end
 
   add_foreign_key "votes", "users"
   add_foreign_key "votes", "works"
-  add_foreign_key "works", "users"
 end

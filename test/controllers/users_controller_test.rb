1ecfebbb2acd28e1409fcee4d9cb20252fc66f8a
Karis Kim <ouniiq@hotmail.com>
2018-10-31 08:33:18 -0700

    refactored tests to have a logged in user and guests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,26 +2,50 @@ require 'test_helper'
 
 describe UsersController do
   describe "index" do
-    it "succeeds when there are users" do
+    it "logged in - succeeds when there are users" do
+      user = users(:dan)
+      perform_login(user)
+
       get users_path
 
       # Assert
       must_respond_with :success
     end
 
-    it "succeeds when there are no users" do
-      Vote.destroy_all
-      User.destroy_all
+    it "logged in - succeeds when there are no users other than self" do
 
+      # not sure if you wanted this to work
+      # for guests but instructions said
+      # to only show works root page
+      # to guests so had to modify this test
+      # to only show current user in users#index
+      user = users(:dan)
+      perform_login(user)
+
+      2.times do
+        Vote.last.destroy
+        User.last.destroy
+      end
+      # binding.pry
       get users_path
 
       # Assert
       must_respond_with :success
+    end
+
+    it "redirects to root_path when user is not logged in" do
+      get users_path
+
+      # Assert
+      expect(flash[:result_text]).must_equal "You must be logged in to access that page."
+      must_redirect_to root_path
     end
   end
 
   describe "show" do
-    it "succeeds for existing user ID" do
+    it "logged in - succeeds for existing user ID" do
+      user = users(:dan)
+      perform_login(user)
       # Arrange
       existing_user = users(:dan)
 
@@ -32,21 +56,27 @@ describe UsersController do
       must_respond_with :success
     end
 
-    it "renders 404 not_found for a bogus user ID" do
-      user = users(:dan)
-      id = user.id
-
-      get user_path(id)
-      must_respond_with :success
-
-      Vote.destroy_all
-      User.destroy_all
-
+    it "logged in - renders 404 not_found for a bogus user ID" do
       # Act
-      get user_path(id)
+      user = users(:dan)
+      perform_login(user)
+      
+      get user_path(id: "asdfas")
 
       # Assert
       must_respond_with :missing
+    end
+
+    it "redirects to root_path when user is not logged in" do
+
+      existing_user = users(:dan)
+
+      # Act
+      get user_path(existing_user.id)
+
+      # Assert
+      expect(flash[:result_text]).must_equal "You must be logged in to access that page."
+      must_redirect_to root_path
     end
   end
 end

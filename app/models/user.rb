e19d838a26960666328f9b6fe4b57780498ba808
Divya <divyamohanan@gmail.com>
2018-10-31 00:04:51 -0700

    Alterations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :votes
   has_many :ranked_works, through: :votes, source: :work
+  validates :username, uniqueness: true, presence: true
 
   # def self.build_from_github(auth_hash)
   #  user = User.new
@@ -12,14 +13,13 @@ class User < ApplicationRecord
   #  # Note that the user has not been saved
   #  return user
   # end
-  def self.create_from_github(auth_hash)
+  def self.build_from_github(auth_hash)
      user = User.new
      user.uid = auth_hash[:uid]
      user.provider = auth_hash[:provider]
      user.username = auth_hash['info']['name']
-      # Note that the user has not been saved
+     user.email = auth_hash["info"]["name"]
      return user
   end
 
-  validates :username, uniqueness: true, presence: true
 end

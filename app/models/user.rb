b44d2edbab5c1e108a6e1f338a526e7603613bce
Divya <divyamohanan@gmail.com>
2018-10-31 20:31:19 -0700

    Bug fix committed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,6 @@ class User < ApplicationRecord
   has_many :ranked_works, through: :votes, source: :work
   validates :username, uniqueness: true, presence: true
 
-  # def self.build_from_github(auth_hash)
-  #  user = User.new
-  #  user.uid = auth_hash[:uid]
-  #  user.provider = 'github'
-  #  user.name = auth_hash['info']['name']
-  #  user.email = auth_hash['info']['email']
-  #
-  #  # Note that the user has not been saved
-  #  return user
-  # end
   def self.build_from_github(auth_hash)
      user = User.new
      user.uid = auth_hash[:uid]
@@ -21,5 +11,4 @@ class User < ApplicationRecord
      user.email = auth_hash["info"]["name"]
      return user
   end
-
 end
